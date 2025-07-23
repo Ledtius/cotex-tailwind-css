150a7d668befa30e60e49b96b4b9dc442461e88a
Calet Devia <calebtius@gmail.com>
2025-07-22 21:38:07 -0500

    Keep working in the Header

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <div className="h-screen w-full bg-red-500 flex flex-col justify-between">
-        <Header />
-        <section>sss</section>
-      </div>
+      <Header />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <>
       <header className="w-screen flex justify-between items-center shadow-2xs bg-gray-100 h-fit">
-        <figure className="flex items-center gap-1.5 p-5 size-fit">
+        {/* <figure className="flex items-center gap-1.5 p-5 size-fit">
           <svg
             className="size-10"
             xmlns="http://www.w3.org/2000/svg"
@@ -20,11 +20,23 @@ function Header() {
           >
             <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
           </svg>
-        </button>
-        <nav className="absolute bg-amber-100 w-3/4 h-full p-5">
-          <ul className="font-semibold ">
+        </button> */}
+        <nav className="bg-amber-500 p-2 z-30 h-screen w-4/5">
+          <button className="">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+              <path d="M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z" />
+            </svg>
+          </button>
+
+          <ul className="font-semibold flex flex-col gap-5 text-2xl h-fit">
+            <button>
+              <svg className="size-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                <path d="M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z" />
+              </svg>
+            </button>
+
             <li>
-              <a href="#">Consoultorias</a>
+              <a href="#">Consultorias</a>
             </li>
             <li>
               <a href="#">Productos</a>
@@ -36,7 +48,7 @@ function Header() {
               <a href="#">Contacto</a>
             </li>
           </ul>
-        </nav>{" "}
+        </nav>
       </header>
     </>
   );
