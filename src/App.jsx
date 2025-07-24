5b3852c982111e19ba7985e3698b21fa2a80ac48
Calet Devia <calebtius@gmail.com>
2025-07-23 21:39:30 -0500

    Added the Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Header from "./components/Header";
-
+import Hero from "./components/Hero.jsx";
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
